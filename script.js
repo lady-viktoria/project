bd6201c307421e4f579c712c5e983018089533f0
Vika <example@gmail.com>
2019-12-04 22:11:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//'use strict'
+
+a=5;
+console.log(a);
+
+//console.log(second);
+
+var leftBorderWidth = 1;
+{
+let second = 2;
+}
+const pi = 3.14;
+
+console.log(1/0); 
+console.log("zzz"*9);
+let something;
+console.log(something);
+
+let personу = {
+    name:'John',
+    age: 25,
+    isMarried: false,
+    'property///x': 'x',
+};
+console.log(personу);
+personу['property:y'] = 'y';
+console.log(personу);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[2]);
+
+//alert('Hello World');
+
+//let answer = confirm('Are you here?');
+//console.log(answer);
+
+
+let answer = +prompt('Есть ли Вам 18?', 'Да');
+console.log(answer);
+console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+    //incr++;
+    //decr--;
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
